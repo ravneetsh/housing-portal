4fe2625c94c1e16c705d64a482c4c5cb9b53c91b
EC2 Default User <ec2-user@ip-172-31-9-48.eu-west-1.compute.internal>
2023-11-02 00:05:22 +0000

    commit

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core import validators
 
 class UserSignUpForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
+    email = forms.EmailField(validators=[validators.validate_email])
+    
+    min_length = 2
+    max_length = 40
+    message_lt_min = f"Should have at least {min_length} characters."
+    message_ht_max = f"Should have at most {max_length} characters."
+    name_regex='\A[a-zA-Z]+\Z'
+    name_message='The name accepts only letters!'
+
+    first_name = forms.CharField(validators=[
+        validators.MinLengthValidator(min_length, message_lt_min),
+        validators.MaxLengthValidator(max_length, message_ht_max),
+        validators.RegexValidator(name_regex, name_message)
+        ])
+        
+    last_name = forms.CharField(validators=[
+        validators.MinLengthValidator(min_length, message_lt_min),
+        validators.MaxLengthValidator(max_length, message_ht_max),
+        validators.RegexValidator(name_regex, name_message)
+        ])
+        
     contact_no = forms.RegexField(regex=r'^\+?1?\d{9,15}$',
                                     error_messages = {'invalid': "Contact number must be entered in the format: '+999999999'. Maximum 15 digits are allowed."})
     
