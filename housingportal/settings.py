5917c8d505027cbf8c08278523e871f29178367c
EC2 Default User <ec2-user@ip-172-31-34-35.eu-west-1.compute.internal>
2023-12-08 19:44:12 +0000

    commit

--- housingportal/settings.py
+++ housingportal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't#08)l#&@g7h_2fsr2!-cho334gwh#cpdlx(#gxa1b*$u(v)5s'
+SECRET_KEY=
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
